9214c7a550bb862baa1af15aaeabb5ae53acb666
slimechips <jason.chowcs@gmail.com>
2021-07-26 20:57:04 +0800

    i2c

--- i2c/i2c_reader.py
+++ i2c/i2c_reader.py
@@ -4,34 +4,35 @@ from Raspberry_Pi_Master_for_ESP32_I2C_SLAVE.unpacker import Unpacker
 import time
 
 
-slave_address = 0x04  # slave address is 4
-register = 0x01  # register to read is 0x01
+def read_from_rpi_to_esp32(addr, register):
+  # change 1 of SMBus(1) to bus number on your RPI
+  smbus = SMBus(1)
+  # prepare the data
+  packed = None
+  with Packer() as packer:
+    packer.write(register)
+    packer.write(0x02)
+    packer.end()
+    packed = packer.read()
+
+  raw_list = None
+  smbus.write_bytes(addr, bytearray(packed))
+  time.sleep(0.3)  # wait i2c process the request
+  first = smbus.read_byte(addr)
+  data_length = smbus.read_byte(addr)
+  raw_list = list(smbus.read_bytes(addr, data_length - 2))  # the read_bytes contains the data format: first, length, data, crc8, end bytes
+  raw_list.insert(0, data_length)
+  raw_list.insert(0, first)
+  print(raw_list)
+
+  # let's clean received data
+  unpacked = None
+  with Unpacker() as unpacker:
+      unpacker.write(raw_list)
+      unpacked = unpacker.read()
+  assert unpacked[0] == register
+  return unpacked[1:]
 
 
-def read_from_rpi_to_esp32():
-  try:
-    # change 1 of SMBus(1) to bus number on your RPI
-    smbus = SMBus(1)
-    # prepare the data
-    packed = None
-    with Packer() as packer:
-      packer.write(register)
-      packer.end()
-      packed = packer.read()
-
-    raw_list = None
-    smbus.write_bytes(slave_address, bytearray(packed))
-    time.sleep(0.3)  # wait i2c process the request
-    raw_list = list(smbus.read_bytes(slave_address, 5))  # the read_bytes contains the data format: first, length, data, crc8, end bytes
-    print(raw_list)
-
-    # let's clean received data
-    unpacked = None
-    with Unpacker as unpacker:
-        unpacker.write(raw_list)
-        unpacked = unpacker.read()
-
-    return unpacked
-
-  except Exception as e:
-      print("ERROR: {}".format(e))
+if __name__ == "__main__":
+  print(read_from_rpi_to_esp32(0x04, 0x01))
--- i2c/i2c_sender.py
+++ i2c/i2c_sender.py
@@ -3,8 +3,9 @@ from Adafruit_PureIO.smbus import SMBus
 from Raspberry_Pi_Master_for_ESP32_I2C_SLAVE.unpacker import Unpacker
 from Raspberry_Pi_Master_for_ESP32_I2C_SLAVE.packer import Packer
 from adafruit_extended_bus import ExtendedI2C as I2C
+import math
 
-def sendBytes(addr, bytess):
+def sendBytes(addr, registerCode, bytess):
   max_packet_length = 124
   # i2c = I2C(1)
   # scan = i2c.scan()
@@ -14,34 +15,20 @@ def sendBytes(addr, bytess):
     print(f"byte length: {len(bytess)}")
     if len(bytess) == 0:
       return
-    counter = 0
+    if len(bytess) > max_packet_length:
+      print("Too many bytes")
+      return
     with Packer() as packer:
-      packet_num = (int(len(bytess) / max_packet_length)) + 1
       packer.debug = True
-      packer.write(0x00)
-      packer.write(packet_num)
+      packer.write(registerCode)
+      packer.write(0x01)
+      for b in bytess:
+        packer.write(b)
       packer.end()
       packed = packer.read()
-      smbus.write_bytes(addr, bytearray(packed))
-    while counter < len(bytess):
-      with Packer() as packer:
-        packer.debug = False
-        packer.write(bytess[counter])
-        counter += 1
-        while counter % max_packet_length != 0 and counter < len(bytess):
-          packer.write(bytess[counter])
-          counter += 1
-        packer.end()
-        packed = packer.read()
-        # print("packed: ", packed)
+      # print("packed: ", packed)
       smbus.write_bytes(addr, bytearray(packed))
 
-bytess = bytearray("""We, the citizens of Singapore,
-pledge ourselves as one united people,
-regardless of race, language or religion,
-to build a democratic society
-based on justice and equality
-so as to achieve happiness, prosperity
-and progress for our nation.""", "utf8")
+bytess = [0x01, 0x02]
 
-sendBytes(0x04, bytess)
+sendBytes(0x04, 0x01, bytess)
